a4453339d278eea2d9feb77fec4203c03d2d1426
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2024-01-31 17:31:49 +0100

    fatto l'header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,52 @@
+<header>
+    <div class="rettangolo">
+
+    </div>
+    
+    <nav class="navbar">
+        <div class="container">
+            <img class="logo" src=" {{ Vite::asset('resources/img/dc-logo.png') }} " alt="logo dc">
+            <ul class="text-uppercase d-flex">
+                        <li>
+                            <a href="#">character</a>
+                        </li>
+                        <li>
+                            <a href="#">comics</a>
+                        </li>
+                        <li>
+                            <a href="#">movies</a>
+                        </li>
+                        <li>
+                            <a href="#">tv</a>
+                        </li> 
+                        <li>
+                            <a href="#">games</a>
+                        </li>
+                        <li>
+                            <a href="#">collectible</a>
+                        </li>
+                        <li>
+                            <a href="#">videos</a>
+                        </li>
+                        <li>
+                            <a href="#">fans</a>
+                        </li>
+                        <li>
+                            <a href="#">news</a>
+                        </li> 
+                        <li>
+                            <a href="#">shop</a>
+                    </li>
+                </ul>
+            <form class="d-flex ricerca" role="search">
+                <input class="form-control me-2" type="search"          placeholder="Search" aria-label="Search">
+                
+            </form>
+        </div>
+    </nav>
+    <section class="container-fluid">
+        <img class="jumbotron" src=" {{ Vite::asset('resources/img/jumbotron.jpg') }} " alt="jumbo">
+
+    </section>
+    
+</header>
