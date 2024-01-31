84c393568f2ca3a5c1fbcb85c1ab68e468ac4624
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2024-01-31 18:22:41 +0100

    finito info footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,100 @@
+<footer>
+    <section class="infosu">
+        <section class="container d-flex">
+            <div class="d-flex">
+                <div class="info ">
+                    <ul>
+                        <p>
+                            titolo
+                        </p>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                    </ul>
+                </div>
+                <div class="info ">
+                    <ul>
+                        <p>
+                            titolo
+                        </p>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                    </ul>
+                </div>
+                <div class="info ">
+                    <ul>
+                        <p>
+                            titolo
+                        </p>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                    </ul>
+                </div>
+                <div class="info ">
+                    <ul>
+                        <p>
+                            titolo
+                        </p>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                        <li>
+                            info
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="dc-grande">
+                <img class="logo" src=" {{ Vite::asset('resources/img/dc-logo-bg.png') }} " alt="logo dc">
+            </div>
+        </section>
+
+    </section>
+</footer>
